5fcfa13392cad3dc0c66e1298fa97240393bd7c3
ibonny <ibonny@gmail.com>
2020-11-05 14:20:03 +0000

    Saving changes to get button that sends an event working.

--- src/main/java/com/example/demo/MainView.java
+++ src/main/java/com/example/demo/MainView.java
@@ -2,13 +2,31 @@ package com.example.demo;
 
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
 
 @Route("")
 public class MainView extends VerticalLayout {
+    private static final long serialVersionUID = 1L;
+
+    public void clicked() {
+
+    }
+
     public MainView() {
         H1 header = new H1("This is a test.");
 
         add(header);
+
+        Div clickedMessage = new Div();
+
+        Button button = new Button("Press me", event -> {
+            clickedMessage.setText("Clicked.");
+        });
+
+        add(button);
+
+        add(clickedMessage);
     }
 }
